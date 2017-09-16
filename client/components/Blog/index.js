b8fdaf214b77ae34bc00239daad284236919b659
walterayalae <walterayalae43@gmail.com>
2017-09-16 17:26:29 -0500

    updated website: added latinos in tech event

--- client/components/Blog/index.js
+++ client/components/Blog/index.js
@@ -8,24 +8,24 @@ const Blog = () => (
   <div><br />
     <Card style={{ width: '750px', height: '650px', textAlign: 'center', marginLeft: '420px' }}>
       <CardMedia
-        overlay={<CardTitle title="Austin Diversity Hackathon: Pitch Ideas, Form Teams" subtitle="Wednesday September 13, 2017" />}
+        overlay={<CardTitle title="Latinos in Tech Austin Celebration" subtitle="Thursday September 21, 2017" />}
       >
-        <img style={{ height: 500 }} src="https://scontent-dft4-3.xx.fbcdn.net/v/t1.0-9/20914558_2005168883036133_7509652378930836727_n.png?oh=82605d72d3bb434e545d808adc73d8b1&oe=5A54E09D" alt="" />
+        <img style={{ height: 500 }} src="https://www.ebsco.com/files/blog/img/uploads/National_Hispanic_Heritage_Month_.png" alt="" />
       </CardMedia>
       <CardText>
-      Are you a newbie to programming, and looking for a way to learn from other experienced developers?
-      Are you an experienced developer looking to build something cool while mentoring newcomers to technology / programming?
+         In honor of Hispanic Heritage Month, more than 10 tech companies in
+         Austin have come together to unite the Latinos in Tech community.
       </CardText>
       <CardActions>
         <FlatButton
           label="RSVP"
-          href="https://www.meetup.com/Austin-Hispanic-Hackers-Meetup/events/242928728/"
+          href="https://www.meetup.com/Austin-Hispanic-Hackers-Meetup/events/242928848/"
           secondary
         />
         <FlatButton
           label="More Info"
           primary
-          href="https://www.eventbrite.com/e/3rd-austin-diversity-hackathon-sponsored-by-google-fiber-bazaarvoice-at-capital-factory-tickets-36978567892?aff=erelexpmlt"
+          href="https://latinosintechseptember.splashthat.com/"
         />
       </CardActions>
     </Card>
