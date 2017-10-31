c699078d17d5dc3b6379af9d23990d2e15c739e7
Jason <jason.jiron@gmail.com>
2017-10-31 15:59:42 -0500

    added react-bootstrap/fixed midDiv Icons

--- client/components/HomePage/index.js
+++ client/components/HomePage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import { Link } from 'react-router-dom';
 import styles from './style';
-
+import { Col } from 'react-bootstrap';
 
 const HomePage = () => (
 
@@ -33,6 +33,7 @@ const HomePage = () => (
         </Link>
       </div>
     </div>
+
     <div style={styles.afterHomeDiv}>
       <div style={styles.afterHomeDivOverlay}>
 
@@ -46,35 +47,74 @@ const HomePage = () => (
 
       </div>
     </div><br />
-    <div style={styles.midDiv}>
-      <div style={styles.midSecDiv}>
+
+
+  <div style={styles.midDiv}>
+    <div style={styles.midSecDiv}>
+
+
+
+
+      <Col xs={12} md={4}>
+
         <h2>Help</h2>
+
+
         <i className="fa fa-handshake-o" aria-hidden="true" style={{ fontSize: '130px' }} />
-        <p>Hispanic Hackers helps cultivate diversity in the Tech Community through
-         our informative and thought provoking monthly meetups surrounding the topics
-         in the Latino Community. Our three core values Help, Hack and Hire provide support and
-         connect Latinos accross the Tech Industry in Austin, TX.
-        </p>
-      </div>
-      <div style={styles.midSecDiv}>
+
+            <p>Hispanic Hackers helps cultivate diversity in the Tech Community through
+             our informative and thought provoking monthly meetups surrounding the topics
+             in the Latino Community. Our three core values Help, Hack and Hire provide support and
+             connect Latinos accross the Tech Industry in Austin, TX.
+            </p>
+
+      </Col>
+
+
+
+
+
+      <Col xs={12} md={4}>
+
         <h2>Hack</h2>
-        <i className="fa fa-laptop" aria-hidden="true" style={{ fontSize: '130px' }} />
+
+        <i className="fa fa-laptop" aria-hidden="true" style={{ fontSize: '130px'}} />
         <p>Hispanic Hackers uses technology as a conduit to empower our
          community.During our Hack series we present topics relevant to
          Hispanics in Tech with a goal to learn from each other and provide
          an environment to share opinions and different point of views.
         </p>
-      </div>
-      <div style={styles.midSecDiv}>
+
+      </Col>
+
+      <br />
+
+
+
+
+
+
+      <Col xs={12} md={4}>
+
         <h2>Hire</h2>
+
+
         <i className="fa fa-users" aria-hidden="true" style={{ fontSize: '130px' }} />
         <p> Hispanic Hackers serves as a liaison between companies hoping to hire
          a more diverse workforce and technology professionals searching for the
          next step in their career. Coding Interview Practices and other workforce
          development programs represent some of our leadership in promoting diversity.
         </p>
+      </Col>
+
+
+
+
+
+
+
       </div>
-    </div><br />
+    </div>
   </div>
 );
 
